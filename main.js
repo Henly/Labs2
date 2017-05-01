3c163b2ec1a1a11e952b3207e4b0d8964bd56c8a
sam henly <sam.henly@live.com>
2017-05-01 03:31:39 +0100

    commit 5

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 var http = require('http');
 var fs = require('fs');
 
-/*http.createServer(function(req, res){
+http.createServer(function(req, res){
     fs.readFile('main.html',function (err, data){
         res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
         res.write(data);
         res.end();
     });
-}).listen(process.env.PORT || 3000);*/
+}).listen(process.env.PORT || 3000);
 
 var static = require( 'node-static' ),
     port = 8087,
     http = require( 'http' );
 
-// config
 var file = new static.Server( './public', {
     cache: 3600,
     gzip: true
 } );
 
-// serve
 http.createServer( function ( request, response ) {
     request.addListener( 'end', function () {
         file.serve( request, response );
